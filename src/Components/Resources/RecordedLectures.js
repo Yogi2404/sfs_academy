21ac57ffbe9817340582c921ba3c88dd8487806b
Yogiraj <yogirajsattur@gmail.com>
2024-02-20 23:11:23 +0530

    lecture1recording

--- src/Components/Resources/RecordedLectures.js
+++ src/Components/Resources/RecordedLectures.js
@@ -1,10 +1,34 @@
 import React from "react";
+import lecture1img from "../../Assets/img/lecture/lecture1ss.jpg";
+import "../../Assets/css/RecordedLectures.css";
 
 const RecordedLectures = () => {
-    return(
+
+    const openLinkInNewTab = () => {
+        const linkUrl = 'https://drive.google.com/file/d/1TexmylPwJu0KtwOb_-oUJz5WR5kn811r/view?usp=sharing';
+        window.open(linkUrl, '_blank');
+    };
+
+    return (
         <div>
-            No recorded lectures available yet!
+            <div className="lecture-image">
+                <div class="card">
+                    <div class="card-details">
+                        <p class="text-title">Lecture 1 (18-01-2024)</p>
+                        <p class="text-body">
+                            <img onClick={openLinkInNewTab} src={lecture1img} width={300} height={200} />
+                            <button onClick={openLinkInNewTab} className="playButton">
+                                ▶️
+                            </button>
+                        </p>
+                    </div>
+                    <button onClick={openLinkInNewTab} class="card-button">Revise ▶️</button>
+                </div>
+            </div>
+
         </div>
+
+
     )
 }
 
