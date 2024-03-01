23a0f1d79914dc6e8e14b5291987326a8a248dad
Yogiraj <yogirajsattur@gmail.com>
2024-03-01 20:28:55 +0530

    navbar

--- src/Components/utilities/Navbar.js
+++ src/Components/utilities/Navbar.js
@@ -1,107 +1,107 @@
-import { useRef } from "react";
-import "../../Assets/css/Navbar.css";
-import logo from "../../Assets/img/SFS Academy Logo (14).png";
-import user from "../../Assets/icons/user.svg";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import login from '../../Assets/icons/user.svg';
-import { Model } from "react-model";
-import { useNavigate } from 'react-router-dom';
+// import { useRef } from "react";
+// import "../../Assets/css/Navbar.css";
+// import logo from "../../Assets/img/SFS Academy Logo (14).png";
+// import user from "../../Assets/icons/user.svg";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import login from '../../Assets/icons/user.svg';
+// import { Model } from "react-model";
+// import { useNavigate } from 'react-router-dom';
 
 
-function Navbar({ openLoginModal }) {
-	const navRef = useRef();
+// function Navbar({ openLoginModal }) {
+// 	const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+// 	const showNavbar = () => {
+// 		navRef.current.classList.toggle(
+// 			"responsive_nav"
+// 		);
+// 	};
 
-	const navigate = useNavigate();
+// 	const navigate = useNavigate();
 
-	const handleGotoLogin = () => {
-        // Use the history object to navigate to another page
-        navigate('/login');
-    };
+// 	const handleGotoLogin = () => {
+//         // Use the history object to navigate to another page
+//         navigate('/login');
+//     };
 
-	const handleGotoReviews = () => {
-        // Use the history object to navigate to another page
-        navigate('/reviews');
-    };
+// 	const handleGotoReviews = () => {
+//         // Use the history object to navigate to another page
+//         navigate('/reviews');
+//     };
 
-	const handleGotoCourse = () => {
-        // Use the history object to navigate to another page
-        navigate('/course');
-    };
+// 	const handleGotoCourse = () => {
+//         // Use the history object to navigate to another page
+//         navigate('/course');
+//     };
 
-	const handleGotoResources = () => {
-        // Use the history object to navigate to another page
-        navigate('/resources');
-    };
+// 	const handleGotoResources = () => {
+//         // Use the history object to navigate to another page
+//         navigate('/resources');
+//     };
 
-	const handleGotoTermsAndConditions = () => {
-        // Use the history object to navigate to another page
-        navigate('/termsandconditions');
-    };
+// 	const handleGotoTermsAndConditions = () => {
+//         // Use the history object to navigate to another page
+//         navigate('/termsandconditions');
+//     };
 
-	const handleGotoHomePage = (event) => {
-		event.preventDefault();
-        // Use the history object to navigate to another page
-        navigate('/');
-    };
+// 	const handleGotoHomePage = (event) => {
+// 		event.preventDefault();
+//         // Use the history object to navigate to another page
+//         navigate('/');
+//     };
 
-	const handleFormSubmit = (event) => {
-		event.preventDefault(); // Prevent the default form submission
-		openLoginModal(); // Call your function to open the login modal
-	};
+// 	const handleFormSubmit = (event) => {
+// 		event.preventDefault(); // Prevent the default form submission
+// 		openLoginModal(); // Call your function to open the login modal
+// 	};
 
 
-	return (
-		<nav className="navbar navbar-expand-lg bg-white">
-			<div className="container-fluid sfs-logo">
-				<a className="navbar-brand ms-5 me-5" href="/"><img onClick={handleGotoHomePage} src={logo} alt="Logo"></img></a>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse listnav" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
-						<li className="nav-item dropdown">
-							<div className="dropdown">
-								<button className="dropbtn" onClick={handleGotoHomePage}>Home</button>
-							</div>
-						</li>	
-						<li className="nav-item dropdown">
-							<div className="dropdown">
-								<button className="dropbtn" onClick={handleGotoCourse}>Course</button>
-							</div>
-						</li>
-						<li className="nav-item dropdown">
-							<div className="dropdown">
-								<button className="dropbtn" onClick={handleGotoResources}>Resources</button>
-							</div>
-						</li>
-						<li className="nav-item dropdown">
-							<div className="dropdown">
-								<button className="dropbtn" onClick={handleGotoReviews}>Reviews</button>
-							</div>
-						</li>
-						<li className="nav-item dropdown">
-							<div className="dropdown">
-								<button className="dropbtn" onClick={handleGotoTermsAndConditions}>Terms and Conditions</button>
-							</div>
-						</li>
-					</ul>
-					<form className="d-flex login-btn" role="search" >
-						<button className="btn btn-outline-primary d-flex" type="button" onClick={handleGotoLogin}>
-							Register / Sign In
-						</button>
-					</form>
-				</div>
-			</div>
-		</nav>
+// 	return (
+// 		<nav className="navbar navbar-expand-lg bg-white">
+// 			<div className="container-fluid sfs-logo">
+// 				<a className="navbar-brand ms-5 me-5" href="/"><img onClick={handleGotoHomePage} src={logo} alt="Logo"></img></a>
+// 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+// 					<span className="navbar-toggler-icon"></span>
+// 				</button>
+// 				<div className="collapse navbar-collapse listnav" id="navbarSupportedContent">
+// 					<ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
+// 						<li className="nav-item dropdown">
+// 							<div className="dropdown">
+// 								<button className="dropbtn" onClick={handleGotoHomePage}>Home</button>
+// 							</div>
+// 						</li>	
+// 						<li className="nav-item dropdown">
+// 							<div className="dropdown">
+// 								<button className="dropbtn" onClick={handleGotoCourse}>Course</button>
+// 							</div>
+// 						</li>
+// 						<li className="nav-item dropdown">
+// 							<div className="dropdown">
+// 								<button className="dropbtn" onClick={handleGotoResources}>Resources</button>
+// 							</div>
+// 						</li>
+// 						<li className="nav-item dropdown">
+// 							<div className="dropdown">
+// 								<button className="dropbtn" onClick={handleGotoReviews}>Reviews</button>
+// 							</div>
+// 						</li>
+// 						<li className="nav-item dropdown">
+// 							<div className="dropdown">
+// 								<button className="dropbtn" onClick={handleGotoTermsAndConditions}>Terms and Conditions</button>
+// 							</div>
+// 						</li>
+// 					</ul>
+// 					<form className="d-flex login-btn" role="search" >
+// 						<button className="btn btn-outline-primary d-flex" type="button" onClick={handleGotoLogin}>
+// 							Register / Sign In
+// 						</button>
+// 					</form>
+// 				</div>
+// 			</div>
+// 		</nav>
 
-	);
-}
+// 	);
+// }
 
-export default Navbar;
+// export default Navbar;
